f3a945ed26681a80d936600757208e13fca9db3c
kolya130899 <kolyawote75@gmail.com>
2020-03-06 12:26:03 +0200

    Updates

---
+++ precache-manifest.c02803dd3e098aae8f079f7a721168c3.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a2c953cf9c4f1654af1eb41975c95d0",
+    "url": "/insilico/index.html"
+  },
+  {
+    "revision": "e66d3bfc8cd1ec93800d",
+    "url": "/insilico/static/css/main.00940c77.chunk.css"
+  },
+  {
+    "revision": "fad1f707ff0f80f33de2",
+    "url": "/insilico/static/js/2.b20fa4a0.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/insilico/static/js/2.b20fa4a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e66d3bfc8cd1ec93800d",
+    "url": "/insilico/static/js/main.a88fb43c.chunk.js"
+  },
+  {
+    "revision": "490b6a6ac57f08b3fe31",
+    "url": "/insilico/static/js/runtime-main.3b17fa5d.js"
+  },
+  {
+    "revision": "ade91f473255991f410f61857696434b",
+    "url": "/insilico/static/media/Montserrat-Bold.ade91f47.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/insilico/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "3ed9575dcc488c3e3a5bd66620bdf5a4",
+    "url": "/insilico/static/media/OpenSans-Regular.3ed9575d.ttf"
+  },
+  {
+    "revision": "bb5ae98e4ce1a64042093dc235c305ed",
+    "url": "/insilico/static/media/Raleway-Medium.bb5ae98e.ttf"
+  },
+  {
+    "revision": "9942588a6c84f959132556d99e83ded6",
+    "url": "/insilico/static/media/Raleway-Regular.9942588a.ttf"
+  },
+  {
+    "revision": "d40214b23a78789d301b000571861df5",
+    "url": "/insilico/static/media/card-bg1.d40214b2.png"
+  },
+  {
+    "revision": "f2bdae9c148ce45cccab5496ea050f70",
+    "url": "/insilico/static/media/card-bg2.f2bdae9c.png"
+  },
+  {
+    "revision": "dc29500b12763cb8b6390b28732e4ed8",
+    "url": "/insilico/static/media/card-bg3.dc29500b.png"
+  },
+  {
+    "revision": "bfee8bc642d6fdc3a94e71cf81948efd",
+    "url": "/insilico/static/media/open-toggler.bfee8bc6.png"
+  }
+]);
